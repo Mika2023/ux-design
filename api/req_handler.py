2ad8ffd1679d83e0f31362604cf9ae73d68b7643
Mika2023 <yajuliyaserysheva2005@gmail.com>
2025-02-18 23:37:42 +0300

    vercel

--- api/req_handler.py
+++ api/req_handler.py
@@ -1,13 +1,11 @@
 # api/bot.py
 import telebot
-import os
-import json
 from flask import Flask, request
+
+app = Flask(__name__)
 
 TOKEN = "7711604335:AAF-WmHthrkkIrzOyXhz07lkYFP4DqsxjuA"
 bot = telebot.TeleBot(TOKEN)
-
-app = Flask(__name__)
 
 @app.route(f'/{TOKEN}', methods=['POST'])
 def webhook():
@@ -16,9 +14,9 @@ def webhook():
     bot.process_new_updates([update])
     return 'OK', 200
 
-@app.route('/')
-def index():
-    return 'Telegram Bot is running!'
+# @app.route('/')
+# def index():
+#     return 'Telegram Bot is running!'
 
 if __name__ == '__main__':
     app.run(debug=True)
